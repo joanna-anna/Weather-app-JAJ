14435d671069613cda9ff6aca9f47fbd3391a17d
joanna-anna <jo.an.jr.promo@gmail.com>
2022-11-06 23:57:35 +0100

    Submit the JS

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector(`#temperature`);
+  let cityElement = document.querySelector(`#city`);
+  let descriptionElement = document.querySelector(`#description`);
+  let humidityElement = document.querySelector(`#humidity`);
+  let windElement = document.querySelector(`#wind`);
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.main.wind.speed);
+}
 
+let apiKey = "d0fo1c2387c00a7200tda8b3e35c0794";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=NewYork&appid=${apiKey}&units=metric`;
+console.log(apiUrl);
+
+axios.get(apiUrl).then(displayTemperature);
