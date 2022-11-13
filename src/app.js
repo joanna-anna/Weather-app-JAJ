44d9f14ff66fda1381b53e7c6ff1888420c926bb
joanna-anna <jo.an.jr.promo@gmail.com>
2022-11-13 19:31:42 +0100

    Recoding JS

--- src/app.js
+++ src/app.js
@@ -1,18 +1,3 @@
-function displayTemperature(response) {
-  let temperatureElement = document.querySelector(`#temperature`);
-  let cityElement = document.querySelector(`#city`);
-  let descriptionElement = document.querySelector(`#description`);
-  let humidityElement = document.querySelector(`#humidity`);
-  let windElement = document.querySelector(`#wind`);
-  temperatureElement.innerHTML = Math.round(response.data.main.temp);
-  cityElement.innerHTML = response.data.name;
-  descriptionElement.innerHTML = response.data.weather[0].description;
-  humidityElement.innerHTML = response.data.main.humidity;
-  windElement.innerHTML = Math.round(response.data.main.wind.speed);
-}
-
 let apiKey = "d0fo1c2387c00a7200tda8b3e35c0794";
-let apiUrl =´https://api.shecodes.io/weather/v1/current?query=NewYork&key=${apiKey}&units=metric`;
-console.log(apiUrl)
-
-axios.get(apiUrl).then(displayTemperature);
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=NewYork&key=${apiKey}&units=metric`;
+console.log(apiUrl);
