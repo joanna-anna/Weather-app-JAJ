bf3be34c963b1fb3e4d6679776c3779852a66613
joanna-anna <jo.an.jr.promo@gmail.com>
2022-11-13 22:32:14 +0100

    Update the date

--- src/app.js
+++ src/app.js
@@ -1,12 +1,37 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minuts = date.getMinutes();
+  if (minuts < 10) {
+    minuts = `0${minuts}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tusday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minuts}`;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
-  let humidityElement = document.querySelector("#humidityy");
-  let windElement = document.querySelector("#windd");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
   humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
 }
 
 let apiKey = "d0fo1c2387c00a7200tda8b3e35c0794";
