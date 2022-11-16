084d23f2282c4e874a6bb34679c70308f778a591
joanna-anna <jo.an.jr.promo@gmail.com>
2022-11-16 17:32:05 +0100

    Remove function showUser and added  description  element

--- src/app.js
+++ src/app.js
@@ -27,12 +27,14 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let descriptionElement = document.querySelector("#description");
   let iconElement = document.querySelector("#icon");
 
   celsiusTemperature = response.data.temperature.current;
 
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
   cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.time * 1000);
