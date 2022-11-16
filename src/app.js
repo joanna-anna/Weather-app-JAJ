c2236ff29681e25472c39c99474234aa7e7cb587
joanna-anna <jo.an.jr.promo@gmail.com>
2022-11-16 19:13:31 +0100

    Fixed units - HTML und JS

--- src/app.js
+++ src/app.js
@@ -57,19 +57,23 @@ function handleSubmit(event) {
   search(cityInputElement.value);
 }
 
-function displayFahrenheitTimemperatur(event) {
+function displayFahrenheitTemperatur(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
+
   celsiusLink.classList.remove("active");
   fahrenheitLink.classList.add("active");
+
   let fahrenheiTemperature = (celsiusTemperature * 9) / 5 + 32;
   temperatureElement.innerHTML = Math.round(fahrenheiTemperature);
 }
 
-function displayCelsiusTimemperatur(event) {
+function displayCelsiusTemperatur(event) {
   event.preventDefault();
+
   celsiusLink.classList.add("active");
   fahrenheitLink.classList.remove("active");
+
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
@@ -80,8 +84,9 @@ let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", displayFahrenheitTimemperatur);
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperatur);
 
 let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", displayCelsiusTimemperatur);
+celsiusLink.addEventListener("click", displayCelsiusTemperatur);
+
 search("Görlitz");
